1ca6737c2fe6c8ab7101953c16504c8ee60eff9c
Hongyi Syue <homeyxue@outlook.com>
2024-04-01 23:36:22 -0700

    reset modal when close

--- src/components/Modal/testModal.tsx
+++ src/components/Modal/testModal.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Property } from '../../utils/interface/interface';
 // import './testModal.css'
 
@@ -10,15 +10,34 @@ interface props {
 
 export default function TestModal({ open, onClose, onSubmit }: props) {
 
+  useEffect(() => {
+    if (!open) {
+      // Reset detail state when modal is closed
+      setDetail({
+        location: undefined,
+        type: undefined,
+        year: undefined,
+        size: undefined,
+        room: undefined,
+        bathroom: undefined,
+        den: false,
+        ac: false,
+        parking: undefined,
+        MOA: undefined,
+        price: undefined
+      });
+    }
+  }, [open]);
+
   const [detail, setDetail] = useState<Property>({
-    location: '',
+    location: undefined,
     type: undefined,
     year: undefined,
     size: undefined,
     room: undefined,
     bathroom: undefined,
-    den: undefined,
-    ac: undefined,
+    den: false,
+    ac: false,
     parking: undefined,
     MOA: undefined,
     price: undefined
@@ -38,10 +57,11 @@ export default function TestModal({ open, onClose, onSubmit }: props) {
     setDetail((prev) => { return { ...prev, [name]: value } })
   }
 
-  const sumbit = () => {
-    onSubmit(detail)
+  const sumbit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    onSubmit(detail);
+    onClose();
   }
-  // if (!open) return null
 
   return (
     <dialog id='myModal' className='modal' open={open}>
@@ -52,41 +72,41 @@ export default function TestModal({ open, onClose, onSubmit }: props) {
         <div className="row" style={{ display: 'flex', flexDirection: 'row' }}>
           <div className="col" style={{ width: 'fit-content' }}>
             <h3>Location: </h3>
-            <input name='location' type='text' onChange={handleChange} />
+            <input name='location' type='text' value={detail.location} onChange={handleChange} />
             <h3>Property Type: </h3>
-            <select name='type' onChange={handleChange}>
+            <select name='type' value={detail.type} onChange={handleChange}>
               <option value='Please select'></option>
               {typeOptions.map(option => (
                 <option value={option}>{option}</option>
               ))}
             </select>
             <h3>Year: </h3>
-            <input name='year' type='number' onChange={handleChange} />
+            <input name='year' type='number' value={detail.year} onChange={handleChange} />
             <h3>Size: </h3>
-            <input name='size' type='number' onChange={handleChange} />
+            <input name='size' type='number' value={detail.size} onChange={handleChange} />
             <h3>Room: </h3>
-            <input name='room' type='number' onChange={handleChange} />
+            <input name='room' type='number' value={detail.room} onChange={handleChange} />
             <h3>Bathroom: </h3>
-            <input name='bathroom' type='number' onChange={handleChange} />
+            <input name='bathroom' type='number' value={detail.bathroom} onChange={handleChange} />
           </div>
           <div className="col" style={{ width: 'fit-content' }}>
             <h3>Den: </h3>
-            <input name='den' type='checkbox' onChange={handleChange} />
+            <input name='den' type='checkbox' checked={detail.den} onChange={handleChange} />
             <h3>AC: </h3>
-            <input name='ac' type='checkbox' onChange={handleChange} />
+            <input name='ac' type='checkbox' checked={detail.ac} onChange={handleChange} />
             <h3>Parking: </h3>
-            <input name='parking' type='number' onChange={handleChange} />
+            <input name='parking' type='number' value={detail.parking} onChange={handleChange} />
             <h3>MOA: </h3>
-            <input name='moa' type='number' onChange={handleChange} />
+            <input name='moa' type='number' value={detail.MOA} onChange={handleChange} />
             <h3>Price: </h3>
-            <input name='price' type='number' onChange={handleChange} />
+            <input name='price' type='number' value={detail.price} onChange={handleChange} />
           </div>
         </div>
+        <div className="footer col">
+          <button className="button left-button" type="button" onClick={onClose}>X</button>
+          <button className="button right-button" type="submit">Submit</button>
+        </div>
       </form>
-      <div className="footer col">
-        <button className="button left-button" onClick={onClose}>X</button>
-        <button className="button right-button" onClick={sumbit}>Submit</button>
-      </div>
     </dialog>
   )
 }
--- src/components/pages/home.tsx
+++ src/components/pages/home.tsx
@@ -13,7 +13,6 @@ export default function Home() {
 
     const handleAddRecord = (v: Property) => {
         setRows(v); // Custom setter usage
-        setIsOpen(false);
     }
 
     const handleRemoveRecord = (id: GridRowId) => {
